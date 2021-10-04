e95873482798abca88e455f20c36f9fb41f2cfdb
activeliang <activeliang@gmail.com>
2021-10-04 17:56:16 +0800

    change endpoint

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  upstream: 'http://ticom.hongliang.fun/api/v1/gmail_cb',
+  upstream: 'http://ticom.hongliang.fun/api/v1/products/gmail_cb',
   ak: 'e8562670b6c840688f34044309b0fdd0',
   domain: 'jcz.onl',
   maxSize: 1400 * 1024, // 1.4MB, because upstream can only receive 2M at most with base64 encoding
